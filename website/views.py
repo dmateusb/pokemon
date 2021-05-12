2124b53c07ffbb3ac3a04fc6909319356014815a
David Mateus <dmateusb@unal.edu.co>
2021-05-12 05:48:20 -0500

    Add comments

--- website/views.py
+++ website/views.py
@@ -4,8 +4,7 @@ from api.models import *
 from api.util import *
 from .util.data import *
 
-# Create your views here.
-
+#Check if the pokemon to consult is already in the database
 
 def pokemon_in_database(id):
     try:
@@ -14,6 +13,7 @@ def pokemon_in_database(id):
         listing = None
     return True if listing != None else False
 
+#Search pokemon, specie and stats in the Pokeapi
 
 def search_pokemon(id):
     pokemon_data = Data()
@@ -21,12 +21,15 @@ def search_pokemon(id):
     insert_pokemon_data(pokemon_data)
     return pokemon_data
 
+#Get the pokemon, specie and stats from the local database
 
 def get_pokemon(id):
     pokemon_data = Data()
     pokemon_data.get_all_data(id)
     return pokemon_data
 
+#Verify what is the kinds of evolution that the pokemon
+#may have
 
 def verify_kind_evolution(pokemon, evolution_chain):
     columnas = []
@@ -65,6 +68,7 @@ def home(request):
 
     return render(request, 'home.html')
 
+#the main methof for search a Pokemon
 
 def search(request):
 
